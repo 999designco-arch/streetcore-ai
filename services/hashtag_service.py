904f34ad4f30875571d5c8cb1f925fb46c4a0cbb
999designco-arch <999design.co@gmail.com>
2026-05-15 08:47:01 -0300

    Criar hashtag_service.py

---
+++ services/hashtag_service.py
@@ -0,0 +1,8 @@
+def gerar_hashtags(tema):
+    return (
+        f"🏷️ HASHTAGS PARA {tema.upper()}\n\n"
+        "#streetgraff #personalizados #camisetaspersonalizadas "
+        "#adesivospersonalizados #canecaspersonalizadas #brindespersonalizados "
+        "#empreendedorismo #pequenosnegocios #lojacriativa #designpersonalizado "
+        "#marketingdigital #negociolocal #produtospersonalizados"
+    )
