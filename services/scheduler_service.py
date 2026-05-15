f5d27c509684b0ef6fb9c421abe10c9e9d8536ff
999designco-arch <999design.co@gmail.com>
2026-05-15 09:21:20 -0300

    Criar scheduler_service.py

---
+++ services/scheduler_service.py
@@ -0,0 +1,10 @@
+def executar_scheduler():
+
+    return (
+        "⏰ Scheduler ativo.\n\n"
+        "Tarefas automáticas futuras:\n"
+        "- postagem automática\n"
+        "- campanhas automáticas\n"
+        "- relatórios automáticos\n"
+        "- alertas IA"
+    )
