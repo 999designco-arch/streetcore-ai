2594472a461cf1a49fdc6bd4117e9550e4bf43c3
999designco-arch <999design.co@gmail.com>
2026-05-15 07:58:50 -0300

    Atualizar o instagram_service.py

--- services/instagram_service.py
+++ services/instagram_service.py
@@ -1,1 +1,9 @@
-# INSTAGRAM SERVICE
+def gerar_post_instagram(tema):
+    return (
+        f"🔥 POST PRONTO SOBRE {tema.upper()}\n\n"
+        "Sua marca precisa aparecer com estilo.\n"
+        "A Street Graff cria camisetas, adesivos, canecas, panfletos e brindes personalizados.\n\n"
+        "📲 Chama no direct e peça seu orçamento.\n\n"
+        "#streetgraff #adesivospersonalizados #camisetaspersonalizada "
+        "#canecaspersonalizada #brindespersonalizados #empreendedorismo"
+    )
